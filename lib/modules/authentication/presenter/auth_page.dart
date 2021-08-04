b4d9d44e8dcdeb77bd92cdbea44e30c7e5bf8f0b
Paulo Victor <pauloalmeidajp652@gmail.com>
2021-08-03 21:20:15 -0300

    Red UI 1.3: ajustes na tela de login

--- lib/modules/authentication/presenter/auth_page.dart
+++ lib/modules/authentication/presenter/auth_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:gameplay/shared/theme/app_colors.dart';
 import 'package:gameplay/shared/theme/app_text_styles.dart';
 import 'package:gameplay/shared/widgets/buttons/button_widget.dart';
-import 'package:gameplay/shared/widgets/buttons/discord_button.dart';
 import 'package:gameplay/shared/widgets/inputs/input_widget.dart';
 
 class AuthPage extends StatefulWidget {
@@ -23,72 +22,74 @@ class _AuthPageState extends State<AuthPage> {
     return Scaffold(
       backgroundColor: AppColors.secondary80,
       body: new SafeArea(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/illustration.png'),
-            new Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 24
+        child: new SingleChildScrollView(
+          physics: new BouncingScrollPhysics(),
+          child: new Column(
+            children: [
+              Image.asset('assets/illustration.png'),
+              new Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24
+                ),
+                child: new Text(
+                  'Conecte-se e organize suas jogatinas',
+                  textAlign: TextAlign.center,
+                  style: AppTextStyles.titleRegular,
+                ),
               ),
-              child: new Text(
-                'Conecte-se e organize suas jogatinas',
-                textAlign: TextAlign.center,
-                style: AppTextStyles.titleHome,
+              new Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 8
+                ),
+                child: new Text(
+                  'Crie grupos para jogar seus games favoritos com seus amigos',
+                  textAlign: TextAlign.center,
+                  style: AppTextStyles.buttonBackground,
+                ),
               ),
-            ),
-            new Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 24,
-                vertical: 16
+              new SizedBox(height: 16),
+              new Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                ),
+                child: new InputWidget(
+                  keyboardType: TextInputType.emailAddress,
+                  controller: _email,
+                  label: 'Email',
+                  icon: Icons.email_rounded,
+                  width: double.maxFinite,
+                ),
               ),
-              child: new Text(
-                'Crie grupos para jogar seus games favoritos com seus amigos',
-                textAlign: TextAlign.center,
-                style: AppTextStyles.trailingBackground,
+              new SizedBox(height: 16),
+              new Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                ),
+                child: new InputWidget(
+                  keyboardType: TextInputType.emailAddress,
+                  controller: _senha,
+                  label: 'Senha',
+                  icon: Icons.lock_rounded,
+                  width: double.maxFinite,
+                  isPass: true,
+                ),
               ),
-            ),
-            new SizedBox(height: 16),
-            new Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 24,
+              new SizedBox(height: 16),
+              new Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24
+                ),
+                child: new ButtonWidget(
+                  label: 'Entrar',
+                  textStyle: AppTextStyles.titleRegular,
+                  background: AppColors.primary,
+                  onTap: () => Modular.to.pushReplacementNamed('/home')
+                ),
               ),
-              child: new InputWidget(
-                keyboardType: TextInputType.emailAddress,
-                controller: _email,
-                label: 'Email',
-                icon: Icons.email_rounded,
-                width: double.maxFinite,
-              ),
-            ),
-            new SizedBox(height: 16),
-            new Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 24,
-              ),
-              child: new InputWidget(
-                keyboardType: TextInputType.emailAddress,
-                controller: _senha,
-                label: 'Senha',
-                icon: Icons.lock_rounded,
-                width: double.maxFinite,
-                isPass: true,
-              ),
-            ),
-            new SizedBox(height: 16),
-            new Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 24
-              ),
-              child: new ButtonWidget(
-                label: 'Entrar',
-                textStyle: AppTextStyles.titleRegular,
-                background: AppColors.primary,
-                onTap: () => Modular.to.pushReplacementNamed('/home')
-              ),
-            ),
-            // new SizedBox(height: 16),
-          ],
+              // new SizedBox(height: 16),
+            ],
+          ),
         ),
       ),
     );
